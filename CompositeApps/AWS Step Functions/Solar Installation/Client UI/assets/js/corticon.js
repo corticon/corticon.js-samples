0157fe9ec630047578f23eac9306082afafc8632
darkakiaa <davidyxiao20@gmail.com>
2020-08-13 13:06:03 -0400

    Css work, form quote step, readme updates

--- CompositeApps/AWS Step Functions/Solar Installation/Client UI/assets/js/corticon.js
+++ CompositeApps/AWS Step Functions/Solar Installation/Client UI/assets/js/corticon.js
@@ -15,8 +15,11 @@ const config = {
   // UI step delay to simulate step functions taking 1-2s per step
   "enable-step-delay": true,
 
+  // 500ms delay range
+  "enable-randomize-delay": true,
+
   // UI step delay in ms
-  "step-delay": 1000,
+  "step-delay": 1500,
 };
 const fieldIds = ['#property-address', 
                   '#property-city',
@@ -89,11 +92,89 @@ function payload() {
   return payload;
 }
 
+// helper to find object from DS output
+function findDSObject(json, type) {
+  return json.Objects.find(function(object) { return object.__metadata["#type"] == type });
+}
+
 // result handler for step function output
 function resultHandler(result) {
-  let quote = result.Objects.find(function(object) { return object.__metadata["#type"] == 'Quote' });
+  let tmp = {
+    "__metadataRoot": {},
+    "Objects": [
+      {
+        "Address": "That Street in",
+        "EstimatedMonthlyGenerated_kWhr": "719.7885",
+        "MonthlyElectricBill": "100",
+        "City": "Boston",
+        "Orientation": "SW",
+        "Shade": "0.97",
+        "Type": "Residential",
+        "State": "MA",
+        "RoofSize": "150",
+        "Value": "400000",
+        "Country": "US",
+        "__metadata": {
+          "#type": "Property",
+          "#id": "Property_id_1"
+        },
+        "EstimatedInstallationCost": "15500",
+        "Age": 11,
+        "MonthlyElectricConsumption": null
+      },
+      {
+        "__metadata": {
+          "#type": "Constants",
+          "#id": "Constants_id_1"
+        },
+        "DefaultInstallationSize": "5000",
+        "DefaultRequiredRoofSpace": "33",
+        "PricePerWatt": "3.1",
+        "Cost_kWhr": "0.2257"
+      },
+      {
+        "__metadata": {
+          "#type": "Rebate",
+          "#id": "Rebate_id_1"
+        },
+        "Flat": "1000",
+        "Percent": "0.31"
+      },
+      {
+        "__metadata": {
+          "#type": "Savings",
+          "#id": "Savings_id_1"
+        },
+        "Resell_kWhr": "0.3057"
+      },
+      {
+        "__metadata": {
+          "#type": "Quote",
+          "#id": "Quote_id_1"
+        },
+        "Value": "9695"
+      }
+    ],
+    "status": "success",
+    "awsRequestId": "062e26e2-b48f-4ce3-acd1-6c6d0b7c023f",
+    "data": {
+      "requireLoan": false
+    }
+  }
+  let property = findDSObject(result, 'Property');
+  let constants = findDSObject(result, 'Constants');
+  let rebate = findDSObject(result, 'Rebate');
+  let quote = findDSObject(result, 'Quote');
 
-  $('#quote-value').text('Your Total is: $' + quote["Value"]);
+  let estimateBreakdown = constants["DefaultInstallationSize"] + 'W * $' + parseFloat(constants["PricePerWatt"]).toFixed(2) + ' ='
+  $('#quote-estimate-breakdown').text(estimateBreakdown);
+  $('#quote-percent-breakdown').text('$' + property["EstimatedInstallationCost"] + ' * ' + rebate["Percent"] + ' =');
+
+  $('#quote-estimate').text('$' + property["EstimatedInstallationCost"]);
+  $('#quote-percent-rebate').text('$' + (parseInt(property["EstimatedInstallationCost"]) * parseFloat(rebate["Percent"])).toFixed(2));
+  $('#quote-flat-rebate').text('$' + rebate["Flat"]);
+
+  $('#quote-final').text(quote["Value"]);
 }
 
 // Start an execution of the configured State Machine, see config at top of file to change state machine arn
@@ -161,14 +242,17 @@ function onAwsStep(jQueryObj, resubmit = false) {
   //only call if manual trigger or first execution
   if (jQueryObj.is('#aws-step') && (resubmit || window.finishedExecution == false)) {
     callStepFunction();
+    cycleAwsStep();
   }
   return jQueryObj;
 }
 
 // Left-side Nav 
 function changeStep() {
-  $('.step-item.selected').removeClass('selected');
-  $(this).addClass('selected')
+  // already selected
+  if($(this).hasClass('selected')) { return; }
+  $('.step-item.selected').removeClass('selected').addClass('complete');
+  $(this).addClass('selected');
 
   $('.form-step.selected').fadeOut().removeClass('selected');
   let target = $('#' + $(this).attr('id').replace(/-title/g,''))
@@ -176,10 +260,35 @@ function changeStep() {
   onAwsStep(target.addClass('selected').fadeIn());
 }
 
+function changeNestedStep(index) {
+  if (index < 1 || index > 6) { return; }
+  //$('#aws-step-title .step-progress-bar.nested .selected').removeClass('selected').addClass('complete');
+  $('#aws-step-title .step-progress-bar.nested li:nth-child(' + index + ')').addClass('selected').addClass('complete');
+  $('.aws-step-img').removeClass('aws-step-1-img aws-step-2-img aws-step-3-img aws-step-4-img aws-step-5-img aws-step-6-img');
+  $('.aws-step-img').addClass('aws-step-' + index + '-img');
+}
+function changeNestedStepClickHandler() {
+  changeNestedStep($(this).index() + 1);
+}
+// Simulate steps completing with delay
+function cycleAwsStep(index=1) {
+  if (index < 1 || index > 6) { return; }
+  changeNestedStep(index);
+  if (config["enable-step-delay"]) {
+    let delay =  config["enable-randomize-delay"] ? config["step-delay"] + Math.random() * 1000 - 500 : config["step-delay"]
+    setTimeout(function(){cycleAwsStep(index + 1)}, delay);
+  } else {
+    cycleAwsStep(index++);
+  }
+}
+
 // Form Controls
 function nextStep() {
   let steps = $('.form-step.selected').nextAll('.form-step');
   if (steps.length) {
+    let stepItem = $('.step-item.selected').removeClass('selected').addClass('complete');
+    stepItem.next().addClass('selected');
+
     $('.form-step.selected').fadeOut().removeClass('selected');
     onAwsStep(steps.eq(0).addClass('selected').fadeIn());
   }
@@ -187,6 +296,9 @@ function nextStep() {
 function prevStep() {
   let steps = $('.form-step.selected').prevAll('.form-step');
   if (steps.length) {
+    let stepItem = $('.step-item.selected').removeClass('selected');
+    stepItem.prev().addClass('selected');
+
     $('.form-step.selected').fadeOut().removeClass('selected');
     onAwsStep(steps.eq(0).addClass('selected').fadeIn());
   }
@@ -204,7 +316,8 @@ $(document).ready(function() {
   window.finishedExecution = false; // flag for step function
 
   // Click Handlers
-  $('.step-progress-bar .step-item').click(changeStep);
+  $('.step-progress-bar .step-item:not(.nested)').click(changeStep);
+  $('.step-progress-bar .step-item.nested').click(changeNestedStepClickHandler);
   $('.form-next').click(nextStep);
   $('.form-prev').click(prevStep);
 
