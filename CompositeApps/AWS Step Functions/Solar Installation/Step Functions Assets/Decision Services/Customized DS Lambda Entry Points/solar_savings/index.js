86cd96c5706562cbd6e953e14ecf62b02d90c8f3
Thierry Ciot <thciot@gmail.com>
2020-08-21 07:59:26 -0400

    Document some more the custom lambda entry points and in particular why we need to manipulate the payload after a parallel state.

--- CompositeApps/AWS Step Functions/Solar Installation/Step Functions Assets/Decision Services/Customized DS Lambda Entry Points/solar_savings/index.js
+++ CompositeApps/AWS Step Functions/Solar Installation/Step Functions Assets/Decision Services/Customized DS Lambda Entry Points/solar_savings/index.js
@@ -4,7 +4,7 @@ This Decison Service Lambda calculates the potential savings on electricity
 In this demo this lambda is directly called after a Parallel Step, where the output is an
 array of valid Decision Service Payloads
 
-This file does not contain a lot of the auto-generated code out of corticon.js for readabiliy sake.
+This file does not contain a lot of the auto-generated code out of corticon.js for readability sake.
 */
 
 const decisionService = require('./decisionServiceBundle');
@@ -24,9 +24,16 @@ exports.handler = async (event, context) => {
         }
     }
     **/
-    
-    body = event[1];
-    body.Objects.push(event[0].Objects[2]);
+
+    // We need to prepare the payload in a specific manner as this Lambda function is executed after the parallel
+    // state.  The output of that state are in an a 2 elements array (1 element per action).
+    // In that parallel state we did 2 things at once:
+    // 1) computed the rebate: the results are in event[0]
+    // 2) got various metrics from the REST Solar price service: these are in event[1]
+    //
+    // So we simply merge these into a single object for consumption by the savings decision service.
+    body = event[1]; // take the whole object
+    body.Objects.push(event[0].Objects[2]); // we only take the object that contains the rebate data
 
     const data = body["additional-data"];
 
