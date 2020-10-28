671bacb5724966051aa912076d80a61440cb555a
Thierry Ciot <thciot@gmail.com>
2020-10-28 11:46:11 -0400

    Update Solar app to Corticon.js V1.1.
    Remove the unneeded custom index.js files + updated the one for savings: With Corticon V1.0 we didn't pass through data that did not belong to rules. So we had to customize  all the index.js files.  However as of Corticon V1.1, all data is passed through, so the only custom step left is for the solar savings rulesheet (as the entry point for that rulesheet needs to create the payload from the output of a parallel step).
    
    Updated all assets from studio.
    Added a zip file with the assets for each version (will be easier to access than restoring from history).

--- CompositeApps/AWS Step Functions/Solar Installation/Step Functions Assets/Decision Services/Customized DS Lambda Entry Points/solar_savings/index.js
+++ CompositeApps/AWS Step Functions/Solar Installation/Step Functions Assets/Decision Services/Customized DS Lambda Entry Points/solar_savings/index.js
@@ -2,14 +2,12 @@
 This Decison Service Lambda calculates the potential savings on electricity
 
 In this demo this lambda is directly called after a Parallel Step, where the output is an
-array of valid Decision Service Payloads
-
-This file does not contain a lot of the auto-generated code out of corticon.js for readability sake.
+array of valid Decision Service Payloads.  The payload to the decision service will be created from
+this array.
 */
 
 const decisionService = require('./decisionServiceBundle');
 
-// This lambda expects an array of valid corticon rules objects, see step function for more details
 exports.handler = async (event, context) => {
     // We need to prepare the payload in a specific manner as this Lambda function is executed after the parallel
     // state.  The output of that state are in an a 2 elements array (1 element per action).
@@ -21,22 +19,7 @@ exports.handler = async (event, context) => {
     const body = event[1]; // take the whole object
     body.Objects.push(event[0].Objects[2]); // we only take the object that contains the rebate data
 
-    const additionalClientUiData = body["additional-data"];
-
-    // This is where you specify various configuration attributes
-    // Note: Errors are always logged no matter what configuration you specify
-    // logLevel: 0: only error gets logged (default), 1: debugging info gets logged
-    // logIsOn: when false, do not log. True by default, you can override dynamically to log data only for certain calls (for example by checking for a specific payload)
-    // logFunction: Used to implement your own logger.  When defined this function is called with a string message to log.
-
-    const configuration = { logLevel: 0 };
-    //const configuration = { logLevel: 1, logIsOn: isLogOnForThisPayload(body), logFunction: myLogger};
-
     // Execute Corticon rules
-    const result = decisionService.execute(body, configuration);
-
-    // preserve the additional client ui data for further steps.
-    result["additional-data"] = additionalClientUiData;
-    
-    return result;
+    const configuration = { logLevel: 0 };
+    return decisionService.execute(body, configuration);
 };
