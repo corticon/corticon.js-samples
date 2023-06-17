5bcd6ff0a90674e45ca963ae955083ec6cfea13f
Thierry Ciot <thciot@gmail.com>
2023-06-17 08:10:36 -0400

    Remove the overwrite on the logFunction. The default implementation goes to the console already.  And, it may give the wrong impression that it is necessary to define this configuration attribute.

--- ClientSideJavaScriptFramework/Browser/Angular/src/app/app.component.ts
+++ ClientSideJavaScriptFramework/Browser/Angular/src/app/app.component.ts
@@ -7,23 +7,22 @@ import { DOCUMENT } from '@angular/common'
 	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-	
+
 	title = 'corticon-test';
 	window: any
 
 	constructor(@Inject(DOCUMENT) private _document:any) {
 		this.window = this._document.defaultView
-	
+
 		const payload = [{
 			"YearsDriving": 1,
 			"DamageWaiver": "Full",
 			"Gender": "male",
 			"Age": 20
 		}];
-			
+
 		const configuration = {
-			logLevel: 1,
-			logFunction: (corticonLogEntry:any) => console.log(corticonLogEntry)
+			logLevel: 1
 		};
 		this.window.corticonEngine.execute(payload,configuration);
 	};
